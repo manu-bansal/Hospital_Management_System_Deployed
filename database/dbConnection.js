ef87dabf9206dc7ac86d6b19e2d34a8c8e92f781
manu <manubansal.kanha@gmail.com>
2024-10-01 15:38:52 +0530

    commit

---
+++ database/dbConnection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+export const dbConnection = ()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM_DEPLOYED"
+    })
+    .then(()=>{
+        console.log("connected to database!");
+    })
+    .catch((err) => {
+        console.log(`some error occured while connecting to databse: ${err}`);
+    })
+}
